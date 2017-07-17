6eeeaefe1ec1471fc4463caac7aebb4bf1d49626
SR <srao24@wisc.edu>
2017-07-17 00:54:20 -0500

    FM_Flair_Bot.py: Added comments, fixed some errors
    Removed use of dictionary as a text file should be sufficient.
    Removed duplicate code that adds submission to file twice.
    Removes bot and user comment when post eventually gets flaired.

--- src/FM_Flair_Bot.py
+++ src/FM_Flair_Bot.py
@@ -13,60 +13,68 @@ def bot_login():
     print("Logged in.")
     return reddit_instance
 
-def run_bot(reddit_instance, dict):
+def run_bot(reddit_instance):
     print("Searching for new posts")
     subreddit=reddit_instance.subreddit('fmgtestsub')
     for submission in subreddit.stream.submissions():
-        if submission.link_flair_text=='help':
+        if submission.link_flair_text=='help': #removing submissions flaired as help
             submission.reply('removal message')
             submission.remove()
-        elif submission.link_flair_text is 'none':
+        elif submission.link_flair_text is 'none': #adding submissions not flaired to text file
             add_to_file(submission)
+        check_if_flaired() #running method to check if previous posts have been flaired
 
 def add_to_file(submission):
-    with open('submissions.txt', 'a') as f:
-        f.write(submission)
-        f.write('\n')
+    #with open('submissions.txt', 'a') as f:#
+        #f.write(submission)#
+        #f.write('\n')#
     with open('submissions.txt', 'r') as f:
         submissions = f.read().splitlines()
-    for submission in subreddit.stream.submissions():
-        if submission in submissions:
-            print('Found old post. Skipping...')
-        else:
-            print('Found new post...')
-            add_to_file(submission)
-            f.write(submission+'\n')
-            submissions.append(submission)
-            submission.reply("flair post within 15 minutes by posting SETFLAIR followed by a space and one of the following choices"
-                             +submission.flair.choices)
-            print('Added new submission ID to list')
-            print('Submission IDs found: ', submissions)
+    for submission in submissions:
+        print('Found new post...')
+        add_to_file(submission)
+        f.write(submission+'\n')
+        #submissions.append(submission)#
+        submission.reply("flair post within 15 minutes by replying to this comment with SETFLAIR followed by a space "
+                         "and one of the following choices: "+submission.flair.choices)
+        print('Added new submission ID to list')
+        #print('Submission IDs found: ', submissions)#
 
 def check_if_flaired():
     f=open('submissions.txt','r+')
-    f.seek(0)
+    f.seek(0) #starting from first submission in the list
+    remove_from_list='FALSE' #counter that stores value TRUE when a user flairs a post within 15 minutes
     for line in f:
         submission=line
         if int(time.time())-submission.created>900:
-            if submission.link_flair_text is 'none':
+            if submission.link_flair_text is 'none': #deleting post if user does not flair post in time
                 submission.reply('removal message')
                 submission.remove()
             else:
-                f.write(line+'\n')
+                all_comments = submission.comments.list() #stores all comments posted on submission in all_comments
+                for comment in all_comments:
+                    if "SETFLAIR" in comment and comment.is_root is 'False':
+                        comment.delete() #if user flairs the post within 15 minutes, bot deletes previous comment
+                        comment.parent.delete() #and users flair reply
+                submission.select(comment[8:]) #flairs post
+                        
 
         else:
-            all_comments=submission.comments.list()
+            all_comments=submission.comments.list() #stores all comments posted on submission in all_comments
             for comment in all_comments:
-                if "SETFLAIR" in comment:
-                    submission.select(comment[8:])
-
-
+                if "SETFLAIR" in comment and comment.is_root is 'False':
+                    submission.select(comment[8:]) #flairs post
+                    comment.delete()
+                    parent = comment.parent()
+                    parent.delete()
+                    remove_from_list='TRUE'
+                if remove_from_list is 'FALSE':
+                    f.write(line+'\n') #writes all submissions back to file (effectively deletes all submissions flaired)
 
 reddit_instance=bot_login()
-dict = {}
 while True:
     try:
-        run_bot(reddit_instance. dict)
+        run_bot(reddit_instance)
     except:
         continue
-    time.sleep(15)
+    time.sleep(15) #makes bot sleep for 15 seconds (can be changed appropriately)
