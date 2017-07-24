a082fd796c2a8d64867f478bd75fd69dec653f9d
Swaraj <srao24@wisc.edu>
2017-07-24 15:28:19 -0500

    FM_Flair_Bot.py: Using SQL to scale to size
    First working model for bot. v1.0

--- src/FM_Flair_Bot.py
+++ src/FM_Flair_Bot.py
@@ -1,82 +1,97 @@
 import praw
-import Config_1
 import time
-import os
-import pickle
-import six
+import sqlite3
+import Config_1
+from prawcore.exceptions import PrawcoreException
 
-def load_dict(path_file):
-    if os.path.isfile(path_file):
-        with open(path_file, 'rb') as f:
-            return pickle.load(f)
-    else:
-        return {}
+reddit = praw.Reddit(
+    client_id=Config_1.client_id,
+    client_secret=Config_1.client_secret,
+    user_agent=Config_1.user_agent,
+    username=Config_1.username,
+    password=Config_1.password)
 
-def save_dict(data, path_file):
-    with open(path_file, 'ab') as f:
-        pickle.dump(data, f)
+subreddit = reddit.subreddit('fmgtestsub')
 
-print("Logging in....")
-reddit_instance=praw.Reddit(client_id=Config_1.client_id,
-                            client_secret=Config_1.client_secret,
-                            user_agent=Config_1.user_agent,
-                            username=Config_1.username,
-                            password=Config_1.password)
-print("Logged in.")
-dict_history=load_dict('submissions.txt')
+conn = sqlite3.connect('id_list.db')
+c = conn.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS Submissions_Commented_On(Submission_ID)')
+conn.commit()
+c.execute('CREATE TABLE IF NOT EXISTS Submissions_To_Ignore(Submission_ID)')
+conn.commit()
+
+print('Opened database successfully.')
+print('Logged in as:', Config_1.username)
+
+
+def send_flair_reminder():
+    c.execute('INSERT INTO Submissions_Commented_On VALUES (?)', (submission.id,))
+    conn.commit()
+    submission.reply('Please remember to flair your post.').mod.distinguish()
+    print('Sent flair reminder.')
+
+
+def help_post():
+    submission.reply('''This post has been removed for help flair.''').mod.distinguish()
+    submission.mod.remove()
+    print('Removed a help post.')
+
+
+def remove_comment():
+    for comment in submission.comments:
+        try:
+            if comment.author.name == reddit.user.me():
+                c.execute('INSERT INTO Submissions_To_Ignore VALUES (?)', (submission.id,))
+                conn.commit()
+                comment.delete()
+                print('Removed my flair reminder.')
+        except PrawcoreException:
+            pass
+
+
+def check_age():
+    print('Checking Age...')
+    removal_msg = '''Post removed for being too old.'''
+    if time.time() - submission.created_utc > 120:
+        submission.reply(removal_msg).mod.distinguish()
+        submission.mod.remove()
+        print('Removed an old post.')
+
+
+def check_flair():
+    print('Checking Flair...')
+    flair_template = subreddit.flair.link_templates
+    flair_list = [flair['text'] for flair in flair_template]
+    if submission.link_flair_text == 'Help':
+        help_post()
+    elif submission.link_flair_text in flair_list:
+        remove_comment()
+    elif submission.link_flair_text not in flair_list:
+        all_comments = submission.comments.list()  # stores all comments posted on submission in all_comments
+        for comment in all_comments:
+            if "SETFLAIR" in comment.body and comment.author is submission.author:
+                flair = comment.body[9:]
+                submission.mod.flair(text=flair, css_class='')
+                comment.mod.remove()  # if user flairs the post within 15 minutes, bot deletes previous comment
+                parent=comment.parent()
+                parent.mod.remove()  # and users flair reply
+        check_age()
+
 
 while True:
-    print("Searching for new posts")
-    subreddit=reddit_instance.subreddit('fmgtestsub')
+    time.sleep(2)
+    print('Searching...')
     for submission in subreddit.new(limit=10):
-        print "new iteration"
-        if submission.link_flair_text=='Help': #removing submissions flaired as help
-            submission.reply('removal message')
-            submission.remove()
-        elif submission.id not in dict_history:
-            dict_history[submission.id] = submission.link_flair_css_class
-            submission.reply("flair post").mod.distinguish()
-        save_dict(dict_history, 'submissions.txt')
-        print "entered check if flaired"
-        dict_keys = list(dict_history)
-        for key in dict_keys:
-            print key
-            submission = reddit_instance.submission(key)
-            if time.time() - submission.created_utc > 30.0:
-                print "time more than 30"
-                all_comments = submission.comments.list()  # stores all comments posted on submission in all_comments
-                for comment in all_comments:
-                    if "SETFLAIR" in comment.body:
-                        flair = comment.body[9:]
-                        submission.mod.flair(text=flair, css_class="Link")
-                        comment.mod.remove()  # if user flairs the post within 15 minutes, bot deletes previous comment
-                        comment.parent.mod.remove()  # and users flair reply
-                        del dict_history[key]
-                        break
-                if submission.link_flair_css_class is not 'Link' and submission.link_flair_text is None:  # deleting post if user does not flair post in time
-                    submission.reply('removal message').mod.distinguish()
-                    submission.mod.remove()
-                    print "deleted post"
-                    del dict_history[key]
-            else:
-                print "time less than 30"
-                if submission.link_flair_css_class is not 'Link' and submission.link_flair_text is None:
-                    print "Not flaired"
-                    all_comments = submission.comments.list()  # stores all comments posted on submission in all_comments
-                    for comment in all_comments:
-                        if "SETFLAIR" in comment.body and comment.is_root is 'False':
-                            flair = comment.body[9:]
-                            submission.mod.flair(text=flair, css_class="Link")
-                            comment.mod.remove()  # if user flairs the post within 15 minutes, bot deletes previous comment
-                            comment.parent.mod.remove()  # and users flair reply
-                            del dict_history[key]
-                            break
-                else:
-                    print "Flaired"
-                    all_comments = submission.comments.list()  # stores all comments posted on submission in all_comments
-                    for comment in all_comments:
-                        if "flair post" in comment.body and comment.is_root is 'True':
-                            comment.mod.remove()  # if user flairs the post within 15 minutes, bot deletes previous comment
-                            del dict_history[key]
-            print "passed all conditions"
-            save_dict(dict_history, 'submissions.txt')
+        print(submission.comments.list())
+        c.execute('SELECT Submission_ID FROM Submissions_Commented_On')
+        submissions_commented_on = set()
+        for row in c.fetchall():
+            submissions_commented_on.add(row[0])
+        c.execute('SELECT Submission_ID FROM Submissions_To_Ignore')
+        submissions_to_ignore = set()
+        for row in c.fetchall():
+            submissions_to_ignore.add(row[0])
+        if submission.id not in submissions_commented_on:
+            send_flair_reminder()
+        if submission.id in submissions_commented_on and submission.id not in submissions_to_ignore:
+            check_flair()
